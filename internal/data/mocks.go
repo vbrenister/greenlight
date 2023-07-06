8fc93c8c4ce98bace4f88e754d5f406657967a68
Victor Brenister <v.brenister@newmotion.com>
2023-07-06 17:34:54 +0300

    added movie model

---
+++ internal/data/mocks.go
@@ -0,0 +1,23 @@
+package data
+
+type MockMovieModel struct{}
+
+func (m MockMovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MockMovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MockMovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MockMovieModel) Delete(id int64) error {
+	return nil
+}
+
+func NewMockMovieModel() MockMovieModel {
+	return MockMovieModel{}
+}
