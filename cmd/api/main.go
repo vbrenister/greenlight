306eea32ff05de632c54b3a64b1c69340959ef89
Victor Brenister <victorbrenister@vandebron.nl>
2024-03-18 14:48:37 +0200

    Send emails

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,10 +6,12 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/vbrenister/greenlight/internal/data"
+	"github.com/vbrenister/greenlight/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -28,26 +30,43 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
 	var conf config
-
+	// Server configs
 	flag.IntVar(&conf.port, "port", 4000, "API server port")
 	flag.StringVar(&conf.env, "env", "development", "Environment (development|staging|production)")
+	// DB configs
 	flag.StringVar(&conf.db.dsn, "dsn", "postgres://greenlight_user:password@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.IntVar(&conf.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&conf.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&conf.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+	// Limiter configs
 	flag.Float64Var(&conf.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&conf.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&conf.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	// SMTP configs
+	flag.StringVar(&conf.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&conf.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&conf.smtp.username, "smtp-username", "8110f75b557333", "SMTP username")
+	flag.StringVar(&conf.smtp.password, "smtp-password", "692eb77ece9880", "SMTP password")
+	flag.StringVar(&conf.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.vbrenister.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -67,6 +86,7 @@ func main() {
 		config: conf,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(conf.smtp.host, conf.smtp.port, conf.smtp.username, conf.smtp.password, conf.smtp.sender),
 	}
 
 	err = app.serve()
