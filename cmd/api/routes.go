b513cfe039c2f3c07597b3486995faa037fc3c82
Victor Brenister <victorbrenister@vandebron.nl>
2024-03-19 14:50:03 +0200

    added metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -25,5 +26,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users/authentication", app.createAuthenticationToken)
 
-	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
+
+	return app.metrics(app.recoverPanic(app.rateLimit(app.authenticate(router))))
 }
